e1e4fad8276c4578255faf7026b9ae7da294a70c
nicolasrubin <nicolasrubin@outlook.com>
2019-04-04 21:44:00 +0200

    added 01_pyramids.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,32 +1,55 @@
+def half_pyramid
+    puts "Ouech alors, choisit la hauteur de ta pyramide:"
+    print ">"
+    choix = gets.chomp.to_i
 
-def get_height
-    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux tu ? "
-    print "> "
-    number = gets.chomp.to_i
-    return number
+    puts "Tadaaaa! une moitié de pyra!" #oui c'est des dollars, c'est Ramsès II, pas la moitié d'une couille
+        n = 1
+        while n <= choix
+            puts ("$" * n).rjust(choix)
+            n += 1
+        end
 end
+    
+def full_pyramid
+    puts "Maintenant la grande soeur! Combien d'étages veux-tu ?"
+    print ">"
+    choix = gets.chomp.to_i 
 
-def half_pyramid(number)
-    puts "Voici la pyramide :"
-    i = 1
-    k = 0
-        if number > 0 && number < 26
-            (number).times do
-                print "#{" " * (number - 1)}"
-                print "#{"#" * i}"
-                puts "#{"#" * k}"
-                number -= 1 
-                i += 1
-                k += 1
-            end
-        else
-            puts "Pick a number between 1 and 25 "
+    puts "Là voici! : "
+    choix.times do |n|
+    print " " *(choix - n)
+    puts "$" * (2 * n + 1)
     end
+    
 end
+
+def wtf_pyramid
+    puts "Une dernière, c'est vraiment parce-que c'est obligé?"
+    print ">"
+    choix = gets.chomp.to_i 
+
+    puts "Voilà la saloperie qui m'a fait perdre des cheveux : "
+    choix.times do |n|
+    print " " * (choix - n)
+    puts "$" * (2 * n + 1)
+    end
+    
+    choix.times do |n| #Y a rien gros écart, je sais, et franchement.....voilà
+        print " " * (n + 2)
+        puts "$" * ((2 + choix) * (2 - 1)-(2*n))
+        end
+    end
 
 def perform
-    number = get_height
     half_pyramid
+    full_pyramid
+    wtf_pyramid
 end
 
+ 
+
 perform
+
+#question de mes collègues m'ont quasi tous posés. Mais où as-tu trouvé rjust?
+#Hey bah voilà https://www.digitalocean.com/community/tutorials/how-to-work-with-string-methods-in-ruby
